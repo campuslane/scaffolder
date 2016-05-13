d301711be3d71ae380ea147dc03feb6f993a1abf
EHCG <EHCG>
2016-05-13 16:54:04 -0700

    initial updates

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,65 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+$config = [
+
+	'table' => [
+
+		'name' => 'posts', 
+		'fields' => [
+
+			'title' => [
+				'name' => 'title', 
+				'type' => 'string', 
+				'size' => 255, 
+				'default' => '', 
+				'index' => 1, 
+				'nullable' => 0,
+				'label' => 'posts', 
+				'instructions_add' => '', 
+				'instructions_edit' => '', 
+				'input' => 'text', 
+				'placeholder' => 'Enter a post...', 
+				'input_class' => 'form-control',
+				'validations' => 'required', 
+				'list_view' => 1,  
+			], 
+
+		], 
+	], 
+
+	'model' => [
+
+		'class_name' => 'Post', 
+		'file_name' => 'Post.php', 
+		'file_path' => 'the_base_path/app/Models/', 
+		'namespace' => 'App\Models',
+
+	], 
+
+	'controller' => [
+
+		'class_name' => 'PostController', 
+		'file_name' => 'PostController.php', 
+		'file_path' => 'the_base_path/app/http/Controllers', 
+		'namespace' => 'App\Http\Controllers',
+		'route' => 'posts', 
+		
+	], 
+
+	'labels' => [
+
+		singular_lowercase => 'post', 
+		singular_titlecase => 'Post', 
+		plural_lowercase => 'posts',
+		plural_titlecase => 'Posts', 
+
+
+	]
+
+	'views_directory' => 'posts', 
+	'migration_name' => '2014_10_12_000000_create_posts_table', 
+
+
+];
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>@yield('title')</title>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+	<div class="container">
+		@yield('content')
+	</div>
+
+	<script src="https://code.jquery.com/jquery-2.2.3.min.js" integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
+
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+	@yield('scripts')
+</body>
+</html>
